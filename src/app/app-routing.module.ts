7fe9f54262993787fa0b9e45002d436968e75dcf
0zhacarias <zhacarias50@outlook.com>
2024-02-20 23:27:10 +0100

    Final do projecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component:HomeComponent
   },
   {
-    path:'conteudo',
+    path:'conteudo/:id',
     component:ConteudosComponent
   }
 ];
--- src/app/pages/conteudos/conteudos.component.ts
+++ src/app/pages/conteudos/conteudos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {dataFake} from '../../data/dataFake'
+import { dataFake } from '../../data/datafake'
 
 @Component({
   selector: 'app-conteudos',
@@ -8,26 +8,27 @@ import {dataFake} from '../../data/dataFake'
   styleUrls: ['./conteudos.component.css']
 })
 export class ConteudosComponent implements OnInit {
-fotoCapa:string='https://play-lh.googleusercontent.com/89pvitgbrXmo4fnifGeFXtIikHzitizxm7fZwhOfeMlCpMHHJh9HTVUZGF90yDkvpP59nA'
-conteudo:string='ssss'
-descricao:string='ssss'
-private id:string |null="0"
+  fotoCapa: string = 'https://play-lh.googleusercontent.com/89pvitgbrXmo4fnifGeFXtIikHzitizxm7fZwhOfeMlCpMHHJh9HTVUZGF90yDkvpP59nA'
+  conteudo: string = 'ssss'
+  descricao: string = 'ssss'
+  private id: string | null = "0"
   constructor(
-    private route:ActivatedRoute
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(value =>
-      this.id=value.get("id")
-     )
-   /*    console.log(value.get("id"))) */
-  // this.setValueToComponent(this.id)
+     /* console.log(value.get("id") */
+      this.id = value.get("id")
+    )
+    /*    console.log(value.get("id"))) */
+   this.setValueToComponent(this.id)
   }
-  setValueToComponent(id:string|null){
-const result=dataFake.filter
-(article=>article.id==id
-
-  )
-console.log(result)
- }
+  setValueToComponent(id: string | null) {
+    const result = dataFake.filter(artigos => artigos.id == id)[0]
+    this.conteudo=result.conteudo
+    this.fotoCapa=result.fotoCapa
+    this.descricao=result.descricao
+  //  console.log(result)
+  }
 }
