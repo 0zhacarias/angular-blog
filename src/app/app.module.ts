57057ed1771141c5a441a5f69f32de567d6eca30
0zhacarias <zhacarias50@outlook.com>
2024-02-20 06:19:26 +0100

    Criação da componente big card e da componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BirCardComponent } from './bir-card/bir-card.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
+import { BigCardComponent } from './big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirCardComponent,
+    MenuTitleComponent,
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bir-card/bir-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bir-card',
+  templateUrl: './bir-card.component.html',
+  styleUrls: ['./bir-card.component.css']
+})
+export class BirCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
