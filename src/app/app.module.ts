1094e0cedad5a52f3e321f731732c4d70f790408
Zacarias Mutue. Milonga <zacarias.milonga@intra.mutue.co.ao>
2024-02-20 09:58:32 +0100

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
+import { SmalCardComponent } from './smal-card/smal-card.component';
+import { BigCardComponent } from './big-card/big-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    SmalCardComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule,
